5e058838e48e284bc5d2103e863a643610af071a
PinkPyp <92897693+PinkPyp@users.noreply.github.com>
2023-10-25 04:14:55 -0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'DJ System for FiveM in VRPEx'
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/styles.css',
+    'html/script.js'
+}
